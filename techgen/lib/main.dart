c4a9e2b76d1db8ca8586206fe8f8f954043732f2
AmanKeswani <keswaniaman137@gmail.com>
2023-02-23 22:24:29 +0530

    basic home page structure completed with nav bar

--- techgen/lib/main.dart
+++ techgen/lib/main.dart
@@ -1,61 +1,112 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_import
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_import, no_leading_underscores_for_local_identifiers
 
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:techgen/screens/SplashScreen.dart';
 
 void main() => runApp(
       const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
+        home: HomePage(),
       ),
     );
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final _height = MediaQuery.of(context).size.height;
+    final _width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            opacity: 50,
-            image: AssetImage("lib/assets/splash_image.jpeg"),
-            fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      bottomNavigationBar: GNav(
+        backgroundColor: Color.fromARGB(255, 234, 230, 230),
+        padding: EdgeInsets.all(10),
+        color: Color(0xFF1E3D33),
+        tabBackgroundColor: Color(0xFFA3DCCA),
+        activeColor: Color(0xFF1E3D33),
+        textStyle: TextStyle(
+          color: Color(0xFF1E3D33),
+          fontSize: 20,
+        ),
+        iconSize: 40,
+        tabs: [
+          GButton(
+            text: "Home Page",
+            icon: Icon(Icons.house_rounded).icon!,
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Tech",
-                  style: TextStyle(
-                    color: Colors.white54,
-                    fontSize: 50,
-                  ),
-                ),
-                Text(
-                  "Gen",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 1, 46, 24),
-                    fontSize: 50,
-                  ),
-                )
-              ],
+          GButton(
+            text: "Your Events",
+            icon: Icon(Icons.calendar_today_rounded).icon!,
+          ),
+          GButton(
+            text: "Upcoming Events",
+            icon: Icon(Icons.assignment_turned_in_rounded).icon!,
+          ),
+          GButton(
+            text: "Profile",
+            icon: Icon(Icons.person_outline_rounded).icon!,
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 10,
             ),
-            SizedBox(height: 20),
-          ],
-        ),
+            height: _height * 0.3,
+            width: _width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+              image: DecorationImage(
+                  image: AssetImage(
+                    'lib/assets/home_bg_image.jpeg',
+                  ),
+                  fit: BoxFit.cover),
+            ),
+            child: Center(
+              child: Text(
+                "Event Updates",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+              child: Container(
+            margin: EdgeInsets.only(left: 10, right: 10, top: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+              color: Colors.red,
+              border: Border(),
+            ),
+            child: ListWheelScrollView(
+              itemExtent: 1,
+              children: [],
+            ),
+          )),
+        ],
       ),
     );
   }
---
+++ techgen/lib/screens/SplashScreen.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateLoginScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            opacity: 50,
+            image: AssetImage("lib/assets/splash_image.jpeg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Tech",
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontSize: 50,
+                  ),
+                ),
+                Text(
+                  "Gen",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 1, 46, 24),
+                    fontSize: 50,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _navigateLoginScreen() {
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        // Add Navigator for new page
+      },
+    );
+  }
+}
