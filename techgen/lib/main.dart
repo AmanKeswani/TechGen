5b32db66f7a66a3aca66ecfa4f11fca856b5404c
AmanKeswani <keswaniaman137@gmail.com>
2023-02-23 20:18:56 +0530

    Scraped previous work and made new splash screen.

--- techgen/lib/main.dart
+++ techgen/lib/main.dart
@@ -1,11 +1,62 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_import
 
 import 'package:flutter/material.dart';
-import 'package:techgen/views/HomePage.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 void main() => runApp(
       const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: SplashScreen(),
       ),
     );
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            opacity: 50,
+            image: AssetImage("lib/assets/splash_image.jpeg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Tech",
+                  style: TextStyle(
+                    color: Colors.white54,
+                    fontSize: 50,
+                  ),
+                ),
+                Text(
+                  "Gen",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 1, 46, 24),
+                    fontSize: 50,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
